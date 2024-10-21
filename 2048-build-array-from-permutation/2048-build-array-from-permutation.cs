5f14c2e6d2b5a7ecbbb71cc25837501cd2aacd3c
noushal <90887683+noushal@users.noreply.github.com>
2024-10-21 10:52:17 +0530

    Time: 0 ms (100%), Space: 51.8 MB (55.93%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] BuildArray(int[] nums) {
+        int n = nums.Length;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++)
+            ans[i] = nums[nums[i]];
+        return ans;
+    }
+}
