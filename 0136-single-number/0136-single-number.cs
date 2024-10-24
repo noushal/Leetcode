71e074b486dd1335ed796c46916b6515cab5be0c
noushal <90887683+noushal@users.noreply.github.com>
2024-10-24 16:05:10 +0530

    Time: 0 ms (100%), Space: 44.9 MB (98.35%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cs
@@ -0,0 +1,15 @@
+// public class Solution {
+//     public int SingleNumber(int[] nums) {
+        
+//     }
+// }
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach (int num in nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
