35ccb4631938c3c186bdbade889bbf3f730555f5
noushal <90887683+noushal@users.noreply.github.com>
2024-10-29 10:40:49 +0530

    Time: 4 ms (77.59%), Space: 62.5 MB (54.71%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cs
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public IList<int> FindDisappearedNumbers(int[] nums) {
-        
         for (int i = 0; i < nums.Length; i++) {
             int index = Math.Abs(nums[i]) - 1;
             if (nums[index] > 0) nums[index] = -nums[index];
