656db15fd6a1a573707fc51efa7a829d7bdaa01f
noushal <90887683+noushal@users.noreply.github.com>
2024-11-20 11:57:06 +0530

    Time: 7 ms (20.22%), Space: 43.8 MB (21.01%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cs
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cs
@@ -1,27 +1,11 @@
 public class Solution {
     public int MinElement(int[] nums) {
-        // for(int i = 0; i < nums.Length; i++){
-        //     if(nums[i] <= 10 && nums.Length != 1 || nums.Length == 1 && nums[i] > 10){
-        //         return 1;
-        //     }else if(nums[i] <= 9){
-        //         return nums[i];
-        //     }else if(nums[i] <= 100){
-        //         return 10;
-        //     }
-        // }
-        // return -1;
+        int minvalue = int.MaxValue;
 
-        // string value = nums.Min().ToString();
-        // value.Split("");
-        // return value;
-
-        int minElement = int.MaxValue;
-
-        foreach (int num in nums) {
-            int digitSum = num.ToString().Sum(c => c - '0');
-            minElement = Math.Min(minElement, digitSum);
+        foreach(int num in nums){
+            int digitsum = num.ToString().Sum(c => c - '0');
+            minvalue = Math.Min(minvalue, digitsum);
         }
-
-        return minElement;
+        return minvalue;
     }
 }
