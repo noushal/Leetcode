b1014cc3880889d7f85e0798ebb48c1a8ec4daf2
noushal <90887683+noushal@users.noreply.github.com>
2024-11-26 17:11:00 +0530

    Time: 1 ms (63.97%), Space: 51.1 MB (54.39%) - LeetHub

--- 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.js
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.js
@@ -4,7 +4,5 @@
  * @return {number}
  */
 var addedInteger = function(nums1, nums2) {
-    let x = 0
-    x = Math.min(...nums2) - Math.min(...nums1)
-    return x
+    return Math.min(...nums2) - Math.min(...nums1)
 };
