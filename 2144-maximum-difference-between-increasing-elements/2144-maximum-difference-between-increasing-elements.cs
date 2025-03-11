609f7ad0ad0c37aff7e2409a27fee8ae128879aa
noushal <90887683+noushal@users.noreply.github.com>
2025-03-11 10:03:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (42.25%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaximumDifference(int[] nums) {
+        int minValue = nums[0];
+        int maxDifference = -1;
+
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] > minValue) {
+                maxDifference = Math.Max(maxDifference, nums[i] - minValue);
+            } else {
+                minValue = nums[i];
+            }
+        }
+
+        return maxDifference;
+    }
+}
